4225c7ff5fab3f50c8868452078ba66fbca8ccf2
Paweł Białek <pawel99bialek@gmail.com>
2025-11-23 21:03:33 +0100

    serwis Scenario

--- src/backend/TtrpgHelperBackend/TtrpgHelperBackend/Services/Resource/ScenarioService.cs
+++ src/backend/TtrpgHelperBackend/TtrpgHelperBackend/Services/Resource/ScenarioService.cs
@@ -1,6 +1,21 @@
-﻿namespace TtrpgHelperBackend.Services.Resource;
+﻿using TtrpgHelperBackend.DTOs.Resource;
 
-public class ScenarioService
+namespace TtrpgHelperBackend.Services.Resource;
+
+public interface IScenarioService
 {
+    Task<GetScenarioDto?> GetScenario(int id, int gameMasterId);
+    Task<IEnumerable<GetScenarioDto>> GetScenarios(int campaignId, int gameMasterId);
+}
+
+public class ScenarioService  : IScenarioService
+{
+    private readonly ApplicationDbContext _db;
+
+    public ScenarioService(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    
     
 }
