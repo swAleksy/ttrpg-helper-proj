fe1aef8bbde5cfeecd3f37c3d18aba155cfa635d
natalia-dukala <ndukala@interia.pl>
2025-12-04 21:52:01 +0100

    - dostęp do endpointów dla guest/zalogowany user + usunięcie przykładowego store'a counter.ts

--- src/frontend/vue-ttrpg-front/src/router/index.ts
+++ src/frontend/vue-ttrpg-front/src/router/index.ts
@@ -1,13 +1,5 @@
-// import { createRouter, createWebHistory } from 'vue-router'
-
-// const router = createRouter({
-//   history: createWebHistory(import.meta.env.BASE_URL),
-//   routes: [],
-// })
-
-// export default router
-
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+import { useAuthStore } from '@/stores/auth'
 
 const routes: RouteRecordRaw[] = [
   {
@@ -24,18 +16,42 @@ const routes: RouteRecordRaw[] = [
     path: '/register',
     name: 'Register',
     component: () => import('@/views/Register.vue'),
-    // component: () => import('@/views/SignUpPage.vue'),
+    // component: () => import('@/views/SignUpPage.vue'), // wersja Adama
   },
   {
     path: '/dashboard',
     name: 'Dashboard',
     component: () => import('@/views/Dashboard.vue'),
+    meta: { requiresAuth: true }, // tylko dla zalogowanych
   },
 ]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+})
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore()
+
+  const isLoggedIn = authStore.isAuthenticated
+
+  // strony tylko dla niezalogowanych
+  const guestOnlyPages = ['Login', 'Register', 'Landing']
+
+  // 1) jeśli user jest zalogowany, a idzie na /, /login lub /register -> przekieruj na dashboard
+  if (isLoggedIn && guestOnlyPages.includes(to.name as string)) {
+    next({ name: 'Dashboard' })
+    return
+  }
+
+  // 2) jeśli trasa wymaga zalogowania, a user NIE jest zalogowany -> logowanie
+  if (to.meta.requiresAuth && !isLoggedIn) {
+    next({ name: 'Login', query: { redirect: to.fullPath } })
+  }
+  else {
+    next()
+  }
 })
 
 export default router
